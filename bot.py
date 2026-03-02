472cc54918d8800e617321e42f19feec0572bb37
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2026-03-02 16:22:10 -0500

    lesson 43 done

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import asyncio
 import logging
 import sys
+
 # from os import getenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 from keyboards import films_keyboard_markup, FilmCallback
 from data import get_films
 from aiogram import F
 from models import Film
 from aiogram.types import URLInputFile
+from data import get_films, add_film
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+
 # from "./.env" import BOT_TOKEN as TOKEN
 from dotenv import load_dotenv
 from os import getenv
@@ -24,11 +27,9 @@ from commands import (
     BOT_COMMANDS,
 )
 
-
 # print(films_keyboard_markup([{'name': "Harry Potter and the Philosopher's Stone", 'description': "Harry Potter and the Philosopher's Stone (also known as Harry Potter and the Sorcerer's Stone in the United States) is a 2001 fantasy film directed by Chris Columbus and produced by David Heyman, from a screenplay by Steve Kloves, based on the 1997 novel of the same name by J. K. Rowling. It is the first instalment in the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.", 'rating': 7.1, 'genre': 'Fantasy', 'actors': ['Daniel Radcliffe', 'Rupert Grint', 'Emma Watson', 'John Cleese', 'Robbie Coltrane', 'Warwick Davis', 'Richard Griffiths', 'Richard Harris', 'Ian Hart', 'John Hurt', 'Alan Rickman', 'Fiona Shaw', 'Maggie Smith', 'Julie Walters'], 'poster': 'https://upload.wikimedia.org/wikipedia/en/7/7a/Harry_Potter_and_the_Philosopher%27s_Stone_banner.jpg'}], 2, 0))
 
 load_dotenv()
-
 
 
 # Bot token can be obtained via https://t.me/BotFather
@@ -49,34 +50,34 @@ dp = Dispatcher()
 #     # await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 #     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
+
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(
-        token=TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await bot.set_my_commands(BOT_COMMANDS)
-    await dp.start_polling(bot)
 
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
 
+
 @dp.message(START_COMMAND)
 async def start(message: Message) -> None:
     await message.answer(
-        f"Вітаю, {message.from_user.full_name}!\n"\
-        "Я перший бот Python розробника Владислава Ліпінського.")
-    
+        f"Вітаю, {message.from_user.full_name}!\n"
+        "Я перший бот Python розробника Владислава Ліпінського."
+    )
+
+
 @dp.message(FILMS_COMMAND)
 async def films(message: Message) -> None:
     data = get_films()
     markup = films_keyboard_markup(films_list=data)
     await message.answer(
         f"Перелік фільмів. Натисніть на назву фільму для отримання деталей.",
-        reply_markup=markup
+        reply_markup=markup,
     )
+
 
 @dp.callback_query(FilmCallback.filter())
 async def callb_film(callback: CallbackQuery, callback_data: FilmCallback) -> None:
@@ -84,16 +85,16 @@ async def callb_film(callback: CallbackQuery, callback_data: FilmCallback) -> No
     film_data = get_films(film_id=film_id)
     film = Film(**film_data)
 
-    text = f"Фільм: {film.name}\n" \
-        f"Опис: {film.description}\n" \
-        f"Рейтинг: {film.rating}\n" \
-        f"Жанр: {film.genre}\n" \
+    text = (
+        f"Фільм: {film.name}\n"
+        f"Опис: {film.description}\n"
+        f"Рейтинг: {film.rating}\n"
+        f"Жанр: {film.genre}\n"
         f"Актори: {', '.join(film.actors)}\n"
-
-    await callback.message.answer_photo(
-        photo=film.poster,
-        caption=text
     )
+
+    await callback.message.answer_photo(photo=film.poster, caption=text)
+
 
 class FilmForm(StatesGroup):
     name = State()
@@ -103,13 +104,6 @@ class FilmForm(StatesGroup):
     actors = State()
     poster = State()
 
-@dp.message(FILM_CREATE_COMMAND)
-async def film_create(message: Message, state: FSMContext) -> None:
-    await state.set_state(FilmForm.name)
-    await message.answer(
-        f"Введіть назву фільму.",
-        reply_markup=ReplyKeyboardRemove(),
-)
 
 @dp.message(FILM_CREATE_COMMAND)
 async def film_create(message: Message, state: FSMContext) -> None:
@@ -117,58 +111,72 @@ async def film_create(message: Message, state: FSMContext) -> None:
     await message.answer(
         f"Введіть назву фільму.",
         reply_markup=ReplyKeyboardRemove(),
-)
+    )
 
 
 @dp.message(FilmForm.name)
 async def film_name(message: Message, state: FSMContext) -> None:
     await state.update_data(name=message.text)
     await state.set_state(FilmForm.description)
+    await message.answer(
+        f"Введіть опис фільму.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @dp.message(FilmForm.description)
 async def film_description(message: Message, state: FSMContext) -> None:
     await state.update_data(description=message.text)
     await state.set_state(FilmForm.rating)
+    await message.answer(
+        f"Вкажіть рейтинг фільму від 0 до 10.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @dp.message(FilmForm.rating)
 async def film_rating(message: Message, state: FSMContext) -> None:
-    await state.update_data(rating=message.text)
+    await state.update_data(rating=float(message.text))
     await state.set_state(FilmForm.genre)
+    await message.answer(
+        f"Введіть жанр фільму.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @dp.message(FilmForm.genre)
 async def film_genre(message: Message, state: FSMContext) -> None:
     await state.update_data(genre=message.text)
     await state.set_state(FilmForm.actors)
+    await message.answer(
+        text=f"Введіть акторів фільму через роздільник ', '\n"
+        + html.bold("Обов'язкова кома та відступ після неї."),
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @dp.message(FilmForm.actors)
 async def film_actors(message: Message, state: FSMContext) -> None:
-    await state.update_data(actors=message.text)
+    await state.update_data(actors=[x for x in message.text.split(", ")])
     await state.set_state(FilmForm.poster)
+    await message.answer(
+        f"Введіть посилання на постер фільму.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 
 @dp.message(FilmForm.poster)
 async def film_poster(message: Message, state: FSMContext) -> None:
-   data = await state.update_data(poster=message.text)
-   await state.clear()
+    data = await state.update_data(poster=message.text)
+    film = Film(**data)
+    add_film(film.model_dump())
+    await state.clear()
+    await message.answer(
+        f"Фільм {film.name} успішно додано!",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
-@dp.message(~F.text.startswith("/"))
-async def echo_handler(message: Message):
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a co"py of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
+# Запуск
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
--- data.py
+++ data.py
@@ -1,9 +1,26 @@
 import json
 
-
-def get_films(file_path:str = "data.json", film_id:int|None = None) -> list[dict] | dict:
-    with open(file_path, 'r', encoding="utf-8") as fp:
+def get_films(
+    file_path: str = "data.json", film_id: int | None = None
+) -> list[dict] | dict:
+    with open("data.json", "r", encoding="utf-8") as fp:
         films = json.load(fp)
         if film_id != None and film_id < len(films):
             return films[film_id]
         return films
+
+
+def add_film(
+    film: dict,
+    file_path: str = "data.json",
+):
+    films = get_films(file_path=file_path, film_id=None)
+    if films:
+        films.append(film)
+        with open(file_path, "w", encoding="utf-8") as fp:
+            json.dump(
+                films,
+                fp,
+                indent=4,
+                ensure_ascii=False,
+            )
